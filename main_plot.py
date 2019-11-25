8123fea4718af737c482a2c39ffb3186f84bc8b4
dw839566105 <839566105@qq.com>
2019-11-25 20:59:54 +0800

    new program

--- main_plot.py
+++ main_plot.py
@@ -1,7 +1,14 @@
+from matplotlib.colors import LogNorm
+from matplotlib.pyplot import *
 import numpy as np
 import scipy, h5py
 import os,sys
 import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d 
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
 
 recon_total = np.empty((0,6))
 truth_total = np.empty((0,4))
@@ -39,6 +46,7 @@ fig_path = './figure'
 if not os.path.exists(fig_path):
     os.mkdir(fig_path)
 
+# fig_0 E and tau_d
 fig=plt.figure(num=0)
 plt.hist2d(recon_total[:, 0], recon_total[:, 5],bins=100)
 plt.colorbar()
@@ -48,3 +56,87 @@ plt.savefig('./figure/E_vs_taud.png')
 plt.ion()
 plt.pause(5)  #显示秒数
 plt.close()
+
+EE = np.arange(0.2, 2.3, 0.1)
+dis = np.sqrt(np.sum(truth_total[:,1:4]**2,1))
+xx = np.max(dis)/10**(1/3)*(np.arange(0,10,1)**(1/3))
+
+E_bias = np.zeros((len(EE)-1, len(xx)-1))
+E_res = np.zeros((len(EE)-1, len(xx)-1))
+x_bias = np.zeros((len(EE)-1, len(xx)-1))
+x_res = np.zeros((len(EE)-1, len(xx)-1))
+tau_bias = np.zeros((len(EE)-1, len(xx)-1))
+tau_res = np.zeros((len(EE)-1, len(xx)-1))
+
+for i in range(len(EE) - 1):
+    for j in range(len(xx) - 1):
+        index = (truth_total[:,0]>EE[i]) & (truth_total[:,0] < EE[i+1]) \
+            & (dis > xx[j]) & (dis > xx[j+1])
+        E_bias[i,j] = np.mean(np.abs(truth_total[index,0] - recon_total[index,0]))/np.sqrt((EE[i]+EE[i+1])/2)
+        E_res[i,j] = np.sqrt(np.var(truth_total[index,0] - recon_total[index,0]))/np.sqrt((EE[i]+EE[i+1])/2)
+        x_bias[i,j] = np.mean(np.abs(truth_total[index,1] - recon_total[index,1]))/np.sqrt((EE[i]+EE[i+1])/2)
+        x_res[i,j] = np.sqrt(np.var(truth_total[index,1] - recon_total[index,1]))/np.sqrt((EE[i]+EE[i+1])/2)
+        tau_bias[i,j] = np.mean(np.abs(tau_real[index] - recon_total[index,5]))/np.sqrt((EE[i]+EE[i+1])/2)
+        tau_res[i,j] = np.sqrt(np.var(tau_real[index] - recon_total[index,5]))/np.sqrt((EE[i]+EE[i+1])/2)
+E_bias[np.isnan(E_bias)] = 0
+E_res[np.isnan(E_res)] = 0
+
+fig=plt.figure(num=1)
+sns.heatmap(pd.DataFrame(E_bias), xticklabels= True, yticklabels= True, 
+            square=True, cmap="YlGnBu")
+plt.xticks(np.arange(len(xx)),np.round(xx,1),color='black',rotation=60)
+plt.yticks(np.arange(len(EE)),np.round(EE,1),color='black',rotation=0)
+plt.xlabel('radius: [m]')
+plt.ylabel('energy: [MeV]')
+plt.title(r'Energy bias/$\sqrt{E}$')
+plt.savefig('./figure/Energy_bias.png')
+
+fig=plt.figure(num=2)
+sns.heatmap(pd.DataFrame(E_res), xticklabels= True, yticklabels= True, 
+            square=True, cmap="YlGnBu")
+plt.xticks(np.arange(len(xx)),np.round(xx,1),color='black',rotation=60)
+plt.yticks(np.arange(len(EE)),np.round(EE,1),color='black',rotation=0)
+plt.xlabel('radius: [m]')
+plt.ylabel('energy: [MeV]')
+plt.title(r'Energy resolution/$\sqrt{E}$')
+plt.savefig('./figure/Energy_res.png')
+
+fig=plt.figure(num=3)
+sns.heatmap(pd.DataFrame(x_bias), xticklabels= True, yticklabels= True, 
+            square=True, cmap="YlGnBu")
+plt.xticks(np.arange(len(xx)),np.round(xx,1),color='black',rotation=60)
+plt.yticks(np.arange(len(EE)),np.round(EE,1),color='black',rotation=0)
+plt.xlabel('radius: [m]')
+plt.ylabel('energy: [MeV]')
+plt.title(r'$x$ bias: [m]')
+plt.savefig('./figure/x_bias.png')
+
+fig=plt.figure(num=4)
+sns.heatmap(pd.DataFrame(x_res), xticklabels= True, yticklabels= True, 
+            square=True, cmap="YlGnBu")
+plt.xticks(np.arange(len(xx)),np.round(xx,1),color='black',rotation=60)
+plt.yticks(np.arange(len(EE)),np.round(EE,1),color='black',rotation=0)
+plt.xlabel('radius: [m]')
+plt.ylabel('energy: [MeV]')
+plt.title(r'$x$ resolution: [m]')
+plt.savefig('./figure/x_res.png')
+
+fig=plt.figure(num=5)
+sns.heatmap(pd.DataFrame(tau_bias), xticklabels= True, yticklabels= True, 
+            square=True, cmap="YlGnBu")
+plt.xticks(np.arange(len(xx)),np.round(xx,1),color='black',rotation=60)
+plt.yticks(np.arange(len(EE)),np.round(EE,1),color='black',rotation=0)
+plt.xlabel('radius: [m]')
+plt.ylabel('energy: [MeV]')
+plt.title(r'$\tau_d$ bias: [ns]')
+plt.savefig('./figure/tau_bias.png')
+
+fig=plt.figure(num=6)
+sns.heatmap(pd.DataFrame(tau_res), xticklabels= True, yticklabels= True, 
+            square=True, cmap="YlGnBu")
+plt.xticks(np.arange(len(xx)),np.round(xx,1),color='black',rotation=60)
+plt.yticks(np.arange(len(EE)),np.round(EE,1),color='black',rotation=0)
+plt.xlabel('radius: [m]')
+plt.ylabel('energy: [MeV]')
+plt.title(r'$\tau_d$ resolution: [ns]')
+plt.savefig('./figure/tau_res.png')
--- test/test1.py
+++ test/test1.py
@@ -1,8 +1,11 @@
-import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
-
-data = np.random.rand(10000,2)
-# plt.hist2d(data[:, 0], data[:, 1],bins=100)
-# plt.scatter(data[:, 0], data[:, 1], c=data[:,1])
-plt.colorbar()
+import pandas as pd
+import matplotlib.pyplot as plt
+a = np.random.rand(4,3)
+#二维的数组的热力图，横轴和数轴的ticklabels要加上去的话，既可以通过将array转换成有column
+#和index的DataFrame直接绘图生成，也可以后续再加上去。后面加上去的话，更灵活，包括可设置labels大小方向等。
+sns.heatmap(pd.DataFrame(a))
+#sns.heatmap(np.round(a,2), annot=True, vmax=1,vmin = 0, xticklabels= True, yticklabels= True, 
+#            square=True, cmap="YlGnBu")
 plt.show()
